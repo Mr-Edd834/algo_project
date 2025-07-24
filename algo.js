7221b27a16e958a310eb393c8f1ff41e40419373
Mr-Edd834 <kagorieddy76@gmail.com>
2025-07-25 00:25:22 +0300

    adding vowel counter

--- algo.js
+++ algo.js
@@ -15,4 +15,21 @@ const form= document.getElementById("putsentence");
 form.addEventListener("submit", function(e) {
     e.preventDefault(); // prevent the form from submitting
     analyzeSentence(); // call the function to analyze the sentence
+    countVowels(); // call the function to count vowels
 });
+
+// adding vowelcounter
+
+function countVowels() {
+    const sentence = sentenceInput.value.trim(); // get the input text
+    let vowelCount=0;
+    const vowels= "aeiouAEIOU";
+  for (let i = 0; i < sentence.length; i++) {
+        if (vowels.includes(sentence[i])) {
+            vowelCount++;
+        }
+    }
+
+document.getElementById("vowelCount").innerHTML = `Number of vowels: ${vowelCount}`;
+}
+
